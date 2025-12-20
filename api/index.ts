7241889ad4cd12ed2aaede7fdf904671ad7c723c
Borda Yashasvi <bordayashvi81@gmail.com>
2025-12-20 22:54:16 +0530

    sw

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,3 @@
 import app from "../src/app";
-import { IncomingMessage, ServerResponse } from "http";
 
-// Vercel will call this default export for requests to /api
-export default function handler(req: IncomingMessage & { url?: string, method?: string, headers?: any }, res: ServerResponse & { setHeader?: any }) {
-    // `app` is an Express app. Express expects `req` and `res` compatible with Node's http objects.
-    // We can directly call it.
-    // @ts-ignore
-    return (app as any)(req, res);
-}
+export default app;
