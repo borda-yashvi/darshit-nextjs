930ea0c5281120f42786e5f0ef1f919ab2584197
yashasvi <bordayashvi81@gmail.com>
2025-12-30 21:43:52 +0530

    g

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema(
     {
         name: { type: String, required: true },
-        email: { type: String, required: true, unique: true, index: true },
+        email: { type: String, unique: true, index: true },
         password: { type: String },
         mobile: { type: String },
         devices: [
